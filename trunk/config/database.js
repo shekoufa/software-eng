171a1cc04bb18dc79c98b9d6661a5fe46fabe20f
Navid Shekoufa <shekoufa.navid@gmail.com>
2015-11-15 17:28:04 -0330

    Almost finished first iteration

---
+++ trunk/config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+//	'url' : 'mongodb://shekoufa:nonoNONO66@apollo.modulusmongo.net:27017/Dy4vehib' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://localhost/ghelp' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
